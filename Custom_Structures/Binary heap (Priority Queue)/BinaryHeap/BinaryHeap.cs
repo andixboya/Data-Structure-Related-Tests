6d0ce4326b0969bb52d7bb2c911987a9992cee19
andixboya <andreyanboyadzhiev@gmail.com>
2019-10-12 21:08:36 +0300

    Adding Binary Heap (Priority Queue) custom structure

---
+++ Custom_Structures/Binary heap (Priority Queue)/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.heap.Count;
+        }
+    }
+
+    public void Insert(T item)
+    {
+
+        this.heap.Add(item);
+        var lastIndex = heap.Count - 1;
+        this.HeapifyUp(item, lastIndex);
+
+    }
+
+    public void HeapifyUp(T item, int index)
+    {
+        var childIndex = index;
+
+        var parentIndex = (index - 1) / 2;
+
+        var parent = this.heap[parentIndex];
+
+        int compare = parent.CompareTo(item);
+
+        if (compare < 0)
+        {
+            SwapNodeValues(parentIndex, childIndex);
+            //we should take the parentIndex because it is next
+            this.HeapifyUp(this.heap[parentIndex], parentIndex);
+        }
+
+    }
+
+    private void SwapNodeValues(int parentIndex, int childIndex)
+    {
+        //here we shift them
+        var parentValue = this.heap[parentIndex];
+        this.heap[parentIndex] = this.heap[childIndex];
+        this.heap[childIndex] = parentValue;
+    }
+
+    public T Peek()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("No elements within the heap.");
+        }
+
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        //returns the last on the line
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+
+        var element = this.heap[0];
+        var lastIndex = this.Count - 1;
+        this.SwapNodeValues(0, this.Count - 1);
+        this.heap.RemoveAt(lastIndex);
+        HeapifyDown(0);
+        return element;
+    }
+
+    private void HeapifyDown(int index)
+    {
+
+        int parentIndex = index;
+
+
+
+        // above count*2 are the children of this element
+        //this is tricky, until which index there are elements who have children (it is a given fact, use it, don`t think about it)
+        while (parentIndex < this.heap.Count / 2)
+        {
+            int childIndex = 2 * parentIndex + 1;
+
+
+
+            //otherwise well go outside of the array`s bound
+            if (childIndex + 1 < this.heap.Count)
+            {
+            //also here, you have to check which node has the bigger value and use it
+                childIndex = GetGreaterIndex(childIndex, childIndex + 1);
+            }
+
+            //here you forgot to add a case when you should swap , not always!
+            if (this.heap[parentIndex].CompareTo(this.heap[childIndex]) < 0)
+            {
+                this.SwapNodeValues(parentIndex, childIndex);
+            }
+
+            //by doing this, it goes to the right until it reaches the bound where the elements have children
+            parentIndex = childIndex;
+        }
+
+    }
+
+    //you`ll need to correct the index here instead of getting the element because later on the index will be used!
+    private int GetGreaterIndex(int leftIndex, int rightIndex)
+    {
+        T leftValue = this.heap[leftIndex];
+        T rightValue = this.heap[rightIndex];
+
+        int compare = leftValue.CompareTo(rightValue);
+
+        return compare < 0 ? rightIndex : leftIndex;
+
+    }
+}
---
+++ Custom_Structures/Binary heap (Priority Queue)/BinaryHeap/Heap.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        ConstructHeap(arr);
+        HeapSort(arr);
+    }
+
+    private static void HeapSort(T[] arr)
+    {
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            //we swap the values
+            SwapNodeValues(arr, 0, i);
+            //then we heapify them down 
+            HeapifyDown(arr, 0, i);
+        }
+
+
+    }
+
+    private static void ConstructHeap(T[] arr)
+    {
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            //here we give the length or the i? 
+            HeapifyDown(arr, 0, arr.Length);
+        }
+
+
+    }
+
+
+    //transfer the methods below from the other class
+    private static int GetGreaterIndex(T[] heap, int leftIndex, int rightIndex)
+    {
+        T leftValue = heap[leftIndex];
+        T rightValue = heap[rightIndex];
+
+        int compare = leftValue.CompareTo(rightValue);
+
+        return compare < 0 ? rightIndex : leftIndex;
+
+    }
+
+    private static void SwapNodeValues(T[] heap, int parentIndex, int childIndex)
+    {
+        //here we shift them
+        var parentValue = heap[parentIndex];
+        heap[parentIndex] = heap[childIndex];
+        heap[childIndex] = parentValue;
+    }
+
+
+    private static void HeapifyDown(T[] heap, int index, int length)
+    {
+
+        int parentIndex = index;
+
+        //here we`ll be decreasing the length?
+        while (parentIndex < length / 2)
+        {
+            int childIndex = 2 * parentIndex + 1;
+
+            //here we`ll be decreasing the length?
+            if (childIndex + 1 < length)
+            {
+                childIndex = GetGreaterIndex(heap, childIndex, childIndex + 1);
+            }
+
+
+            if (heap[parentIndex].CompareTo(heap[childIndex]) < 0)
+            {
+                SwapNodeValues(heap, parentIndex, childIndex);
+            }
+
+
+            parentIndex = childIndex;
+        }
+
+    }
+
+
+    //won`t be necessary? 
+    //public static void HeapifyUp( List<T> heap, T item, int index)
+    //{
+    //    var childIndex = index;
+
+    //    var parentIndex = (index - 1) / 2;
+
+    //    var parent = heap[parentIndex];
+
+    //    int compare = parent.CompareTo(item);
+
+    //    if (compare < 0)
+    //    {
+    //        SwapNodeValues(heap,parentIndex, childIndex);
+    //        //we should take the parentIndex because it is next
+    //        HeapifyUp(heap,heap[parentIndex], parentIndex);
+    //    }
+
+    //}
+
+}
