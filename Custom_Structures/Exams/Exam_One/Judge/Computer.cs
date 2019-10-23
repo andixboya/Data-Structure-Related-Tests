37090c2c53d1463c659e472c8cfa290324fdbb3c
andixboya <andreyanboyadzhiev@gmail.com>
2019-10-23 20:20:56 +0300

    Adding functional solution to "Judge" with 83/100 perfromance tests result.

---
+++ Custom_Structures/Exams/Exam_One/Judge/Computer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class Computer : IComputer
+{
+
+    private Dictionary<int, List<LinkedListNode<Invader>>> byDistance;
+    private LinkedList<Invader> byInsertion;
+
+    private int energy;
+    private int steps = 0;
+
+    public Computer(int energy)
+    {
+        CheckIfBelowZero(energy);
+        this.energy = energy;
+        this.byDistance = new Dictionary<int, List<LinkedListNode<Invader>>>();
+        this.byInsertion = new LinkedList<Invader>();
+    }
+
+    private void CheckIfBelowZero(int energy)
+    {
+        if (energy < 0)
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    public int Energy
+    {
+        get
+        {
+            return this.energy;
+        }
+
+        private set
+        {
+            if (value < 0)
+            {
+                this.energy = 0;
+            }
+            else
+            {
+                this.energy = value;
+            }
+
+        }
+    }
+
+    //it STACKS and is kept for EVERY SINGLE MOVEMENT?
+    public void Skip(int turns)
+    {
+        steps += turns;
+
+        this.byDistance = this.byDistance.Where((x) =>
+        {
+
+            int remDistance = x.Key - this.steps;
+
+            if (remDistance <= 0)
+            {
+                this.Energy -= x.Value.Sum(y => y.Value.Damage);
+                x.Value.ForEach(y => this.byInsertion.Remove(y));
+            }
+
+            return remDistance > 0;
+        }).ToDictionary(x => x.Key, y => y.Value);
+    }
+
+    public void AddInvader(Invader invader)
+    {
+        var node = new LinkedListNode<Invader>(invader);
+        if (!this.byDistance.ContainsKey(invader.Distance))
+        {
+            this.byDistance.Add(invader.Distance, new List<LinkedListNode<Invader>>());
+        }
+        this.byInsertion.AddLast(node);
+        this.byDistance[invader.Distance].Add(node);
+
+    }
+
+    public void DestroyHighestPriorityTargets(int count)
+    {
+        foreach (var linkedListNode in this.byDistance.SelectMany(x => x.Value)
+              .OrderBy(x => x.Value)
+              .Take(count))
+        {
+            this.byInsertion.Remove(linkedListNode);
+        }
+
+        //delete from second 
+        var newDict = this.byDistance.SelectMany(x => x.Value)
+            .OrderBy(x => x.Value)
+            .Skip(count);
+
+        this.byDistance = new Dictionary<int, List<LinkedListNode<Invader>>>();
+        foreach (var item in newDict)
+        {
+            if (!this.byDistance.ContainsKey(item.Value.Distance))
+            {
+                this.byDistance.Add(item.Value.Distance, new List<LinkedListNode<Invader>>());
+            }
+
+            this.byDistance[item.Value.Distance].Add(item);
+        }
+
+
+    }
+
+    public void DestroyTargetsInRadius(int radius)
+    {
+        this.byDistance = this.byDistance
+           .Where(x =>
+           {
+               bool result = x.Key - this.steps > radius;
+
+                //delete from second 
+                if (!result)
+               {
+                   x.Value.ForEach(y => this.byInsertion.Remove(y));
+               }
+                //delete from first
+                return result;
+           })
+           .ToDictionary(x => x.Key, y => y.Value);
+    }
+
+    public IEnumerable<Invader> Invaders()
+    {
+        //we`ll use by order of input here 
+        return this.byInsertion;
+        //return this.byOrdeOfInput
+
+    }
+}
---
+++ Custom_Structures/Exams/Exam_One/Judge/Invader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Invader : IInvader
+{
+    private int damage;
+    private int distance;
+
+    public Invader(int damage, int distance)
+    {
+        this.damage = damage;
+        this.distance = distance;
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return this.damage;
+        }
+        set
+        {
+            this.damage = value;
+        }
+    }
+    public int Distance
+    {
+        get
+        {
+            return this.distance;
+        }
+        set
+        {
+            this.distance = value;
+        }
+    }
+
+    public int CompareTo(IInvader other)
+    {
+        int result = this.distance.CompareTo(other.Distance);
+
+        if (result != 0)
+        {
+            return result;
+        }
+
+        result = other.Damage.CompareTo(this.Damage);
+
+        return result;
+
+    }
+}
